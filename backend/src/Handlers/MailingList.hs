10463559eb6d723e07947cc0001342de8f273c7b
solomon <ssbothwell@gmail.com>
2024-04-14 17:56:05 -0700

    Adds email validation to mailing list handler

--- backend/src/Handlers/MailingList.hs
+++ backend/src/Handlers/MailingList.hs
@@ -3,6 +3,7 @@ module Handlers.MailingList where
 --------------------------------------------------------------------------------
 
 import Control.Concurrent (MVar)
+import Control.Monad (unless)
 import Control.Monad.Except (MonadError)
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader (MonadReader, asks)
@@ -11,12 +12,15 @@ import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Has (Has)
 import Data.Has qualified as Has
 import Data.Text (Text)
+import Data.Text.Encoding qualified as Text.Encoding
+import Effects.MailingList (EmailAddress (EmailAddress))
 import Effects.MailingList qualified as MailingList
 import GHC.Generics (Generic)
 import Hasql.Connection qualified as Connection
 import Log qualified
 import Servant ((:<|>) (..), (:>))
 import Servant qualified
+import Text.Email.Validate qualified as Email
 import Web.FormUrlEncoded (FromForm)
 
 --------------------------------------------------------------------------------
@@ -34,9 +38,9 @@ type MailingListAPI =
 -- Handler
 
 mailingListHandler :: (MonadReader env m, Has Connection.Connection env, MonadError Servant.ServerError m, Log.MonadLog m, MonadIO m) => Servant.ServerT MailingListAPI m
-mailingListHandler MailingListForm {..} = do
-  -- TODO: Validate email address format
-  pid <- MailingList.insertEmailAddress emailAddress
+mailingListHandler (MailingListForm e@(EmailAddress {..})) = do
+  unless (Email.isValid $ Text.Encoding.encodeUtf8 emailAddress) $ Servant.throwError $ Servant.err401 {Servant.errBody = "Invalid Email Address"}
+  pid <- MailingList.insertEmailAddress e
   Log.logInfo "Submited Email Address:" (KeyMap.singleton "email" (show emailAddress))
 
   -- TODO: Very hacky solution until we support htmx.
