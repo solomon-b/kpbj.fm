844873fa497d0d4bbcf6ae064815cf7bc79ece40
solomon <ssbothwell@gmail.com>
2024-04-20 17:51:54 -0700

    Adds ServantError helpers.

--- backend/src/API/User/Current.hs
+++ backend/src/API/User/Current.hs
@@ -4,12 +4,12 @@ module API.User.Current where
 
 import Control.Monad.Catch (MonadThrow (..))
 import Domain.Types.User (User)
+import Errors (throw401')
 import Log qualified
-import Servant qualified
 import Servant.Auth.Server qualified as SAS
 
 --------------------------------------------------------------------------------
 
 handler :: (Log.MonadLog m, MonadThrow m) => SAS.AuthResult User -> m User
 handler (SAS.Authenticated user) = pure user
-handler _ = throwM Servant.err401
+handler _ = throw401'
