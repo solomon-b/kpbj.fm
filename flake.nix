8a2d1be0d26ca9739b0b40553e6f5347697d0f09
solomon <ssbothwell@gmail.com>
2025-05-02 00:31:09 -0700

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic static site";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        in
+        rec {
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs
+              pkgs.nodePackages.serve
+              pkgs.nodePackages.tailwindcss
+            ];
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+        });
+}
