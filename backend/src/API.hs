b4b3f3dd5d1958d80abbd4002b0524d966b7cc50
solomon <ssbothwell@gmail.com>
2025-09-21 14:26:39 -0700

    feat: adds auth handlers.

--- backend/src/API.hs
+++ backend/src/API.hs
@@ -4,6 +4,11 @@ module API where
 
 import API.Get qualified as Root.Get
 import API.User.Login.Get qualified as User.Login.Get
+import API.User.Login.Post qualified as User.Login.Post
+import API.User.Logout.Get qualified as User.Logout.Get
+import API.User.Logout.Post qualified as User.Logout.Post
+import API.User.Register.Get qualified as User.Register.Get
+import API.User.Register.Post qualified as User.Register.Post
 import App qualified
 import App.Config (Environment)
 import Control.Monad.Catch (MonadCatch)
@@ -12,7 +17,11 @@ import Control.Monad.Reader (MonadReader)
 import Data.Has (Has)
 import Data.Proxy (Proxy (..))
 import Data.Text (Text)
+import Domain.Types.DisplayName (DisplayName)
 import Domain.Types.EmailAddress (EmailAddress)
+import Domain.Types.FullName (FullName)
+import Effects.Clock (MonadClock)
+import Effects.Database.Class (MonadDB)
 import Hasql.Pool qualified as HSQL.Pool
 import Log (MonadLog)
 import OpenTelemetry.Trace (Tracer)
@@ -27,7 +36,14 @@ runApi = App.runApp @API server ()
 
 --------------------------------------------------------------------------------
 
-type API = Root.Get.Route :<|> User.Login.Get.Route
+type API =
+  Root.Get.Route
+    :<|> User.Login.Get.Route
+    :<|> User.Login.Post.Route
+    :<|> User.Logout.Get.Route
+    :<|> User.Logout.Post.Route
+    :<|> User.Register.Get.Route
+    :<|> User.Register.Post.Route
 
 --------------------------------------------------------------------------------
 
@@ -36,12 +52,21 @@ server ::
     MonadCatch m,
     MonadLog m,
     MonadUnliftIO m,
+    MonadClock m,
+    MonadDB m,
     MonadReader env m,
     Has HSQL.Pool.Pool env
   ) =>
   Environment ->
   Servant.ServerT API m
-server _env = Root.Get.handler :<|> User.Login.Get.handler
+server _env =
+  Root.Get.handler
+    :<|> User.Login.Get.handler
+    :<|> User.Login.Post.handler
+    :<|> User.Logout.Get.handler
+    :<|> User.Logout.Post.handler
+    :<|> User.Register.Get.handler
+    :<|> User.Register.Post.handler
 
 --------------------------------------------------------------------------------
 
@@ -52,3 +77,23 @@ rootGetLink = Links.safeLink (Proxy @API) (Proxy @Root.Get.Route)
 -- | Route: GET /user/login
 userLoginGetLink :: Maybe Text -> Maybe EmailAddress -> Links.Link
 userLoginGetLink = Links.safeLink (Proxy @API) (Proxy @User.Login.Get.Route)
+
+-- | Route: POST /user/login
+userLoginPostLink :: Maybe Text -> Links.Link
+userLoginPostLink = Links.safeLink (Proxy @API) (Proxy @User.Login.Post.Route)
+
+-- | Route: GET /user/logout
+userLogoutGetLink :: Links.Link
+userLogoutGetLink = Links.safeLink (Proxy @API) (Proxy @User.Logout.Get.Route)
+
+-- | Route: POST /user/logout
+userLogoutPostLink :: Links.Link
+userLogoutPostLink = Links.safeLink (Proxy @API) (Proxy @User.Logout.Post.Route)
+
+-- | Route: GET /user/register
+userRegisterGetLink :: Maybe EmailAddress -> Maybe DisplayName -> Maybe FullName -> Links.Link
+userRegisterGetLink = Links.safeLink (Proxy @API) (Proxy @User.Register.Get.Route)
+
+-- | Route: POST /user/register
+userRegisterPostLink :: Links.Link
+userRegisterPostLink = Links.safeLink (Proxy @API) (Proxy @User.Register.Post.Route)
--- backend/src/API/User/Register/Get.hs
+++ backend/src/API/User/Register/Get.hs
@@ -1,16 +1,16 @@
-module API.User.Login.Get where
+module API.User.Register.Get where
 
 --------------------------------------------------------------------------------
 
-import API.User.Login.Form (template)
+import API.User.Register.Form (template)
 import Component.Frame (loadFrame)
-import Control.Applicative ((<|>))
 import Control.Monad.Catch (MonadCatch)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Reader (MonadReader)
 import Data.Has (Has)
-import Data.Text (Text)
+import Domain.Types.DisplayName (DisplayName)
 import Domain.Types.EmailAddress (EmailAddress)
+import Domain.Types.FullName (FullName)
 import Effects.Observability qualified as Observability
 import Log qualified
 import Lucid qualified
@@ -23,10 +23,10 @@ import Text.HTML (HTML)
 
 type Route =
   "user"
-    :> "login"
-    :> Servant.Header "HX-Current-Url" Text
-    :> Servant.QueryParam "redirect" Text
-    :> Servant.QueryParam "email" EmailAddress
+    :> "register"
+    :> Servant.QueryParam "emailAddress" EmailAddress
+    :> Servant.QueryParam "displayName" DisplayName
+    :> Servant.QueryParam "fullName" FullName
     :> Servant.Get '[HTML] (Lucid.Html ())
 
 --------------------------------------------------------------------------------
@@ -39,11 +39,11 @@ handler ::
     MonadUnliftIO m,
     MonadReader env m
   ) =>
-  Maybe Text ->
-  Maybe Text ->
   Maybe EmailAddress ->
+  Maybe DisplayName ->
+  Maybe FullName ->
   m (Lucid.Html ())
-handler hxCurrentUrl redirectQueryParam emailQueryParam =
-  Observability.handlerSpan "GET /user/login" $ do
-    let loginForm = template emailQueryParam $ hxCurrentUrl <|> redirectQueryParam
-    loadFrame loginForm
+handler emailAddress displayName fullName =
+  Observability.handlerSpan "GET /user/register" $ do
+    let registerForm = template displayName fullName emailAddress Nothing
+    loadFrame registerForm
