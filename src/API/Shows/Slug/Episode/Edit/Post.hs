e10bc59aaa6ef9876d27cc1ab55ba4c8eae7ee33
solomon <ssbothwell@gmail.com>
2025-11-30 11:27:28 -0800

    feat: dont allow file uploads on old episodes.

--- src/API/Shows/Slug/Episode/Edit/Post.hs
+++ src/API/Shows/Slug/Episode/Edit/Post.hs
@@ -8,19 +8,22 @@ module API.Shows.Slug.Episode.Edit.Post where
 
 import {-# SOURCE #-} API (episodesGetLink, hostDashboardGetLink)
 import App.Common (getUserInfo, renderTemplate)
+import Control.Monad (when)
 import Control.Monad.Catch (MonadCatch)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Reader (MonadReader)
 import Data.Either (partitionEithers)
 import Data.Has (Has)
 import Data.Int (Int64)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, isJust)
 import Data.String.Interpolate (i)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.Display (display)
+import Data.Time (UTCTime, getCurrentTime)
 import Domain.Types.Cookie (Cookie)
+import Domain.Types.FileUpload (uploadResultStoragePath)
 import Domain.Types.HxRequest (HxRequest, foldHxReq)
 import Domain.Types.Slug (Slug)
 import Effects.ContentSanitization qualified as Sanitize
@@ -32,6 +35,8 @@ import Effects.Database.Tables.ShowHost qualified as ShowHost
 import Effects.Database.Tables.Shows qualified as Shows
 import Effects.Database.Tables.User qualified as User
 import Effects.Database.Tables.UserMetadata qualified as UserMetadata
+import Effects.FileUpload (stripStorageRoot)
+import Effects.FileUpload qualified as FileUpload
 import Effects.Observability qualified as Observability
 import Hasql.Pool qualified as HSQL.Pool
 import Log qualified
@@ -41,10 +46,10 @@ import OpenTelemetry.Trace (Tracer)
 import Servant ((:>))
 import Servant qualified
 import Servant.Links qualified as Links
+import Servant.Multipart (FileData, FromMultipart, Mem, MultipartForm, fdFileName, fromMultipart, lookupFile, lookupInput)
+import Servant.Multipart qualified
 import Text.HTML (HTML)
 import Text.Read (readMaybe)
-import Web.FormUrlEncoded (Form, FromForm)
-import Web.FormUrlEncoded qualified as Form
 
 --------------------------------------------------------------------------------
 
@@ -68,7 +73,7 @@ type Route =
         :> "edit"
         :> Servant.Header "Cookie" Cookie
         :> Servant.Header "HX-Request" HxRequest
-        :> Servant.ReqBody '[Servant.FormUrlEncoded] EpisodeEditForm
+        :> MultipartForm Mem EpisodeEditForm
         :> Servant.Post '[HTML] (Lucid.Html ())
     )
 
@@ -79,7 +84,10 @@ data EpisodeEditForm = EpisodeEditForm
   { eefTitle :: Text,
     eefDescription :: Maybe Text,
     eefStatus :: Text,
-    eefTracks :: [TrackInfo]
+    eefTracks :: [TrackInfo],
+    -- File uploads (optional - only present if scheduled date is in the future)
+    eefAudioFile :: Maybe (FileData Mem),
+    eefArtworkFile :: Maybe (FileData Mem)
   }
   deriving (Show)
 
@@ -97,73 +105,90 @@ data TrackInfo = TrackInfo
   }
   deriving (Show)
 
-instance FromForm EpisodeEditForm where
-  fromForm form = do
-    title <- Form.parseUnique "title" form
-    description <- Form.parseMaybe "description" form
-    status <- Form.parseUnique "status" form
-    tracks <- parseTracksFromForm form
+instance FromMultipart Mem EpisodeEditForm where
+  fromMultipart multipartData = do
+    title <- lookupInput "title" multipartData
+    let description = either (const Nothing) Just (lookupInput "description" multipartData)
+    status <- lookupInput "status" multipartData
+    -- Parse tracks - using safe parsing that handles multipart correctly
+    let tracks = parseTracksFromMultipart multipartData
+    -- File lookups - these must be done with lookupFile, not lookupInput
+    let audioFile = either (const Nothing) (fileDataToNothing . Just) (lookupFile "audio_file" multipartData)
+        artworkFile = either (const Nothing) (fileDataToNothing . Just) (lookupFile "artwork_file" multipartData)
 
     pure
       EpisodeEditForm
         { eefTitle = title,
           eefDescription = emptyToNothing description,
           eefStatus = status,
-          eefTracks = tracks
+          eefTracks = tracks,
+          eefAudioFile = audioFile,
+          eefArtworkFile = artworkFile
         }
     where
       emptyToNothing :: Maybe Text -> Maybe Text
       emptyToNothing (Just "") = Nothing
       emptyToNothing x = x
 
--- | Parse track data from form fields like tracks[0][title], tracks[0][artist], etc.
--- We try to parse tracks by checking indices starting from 0 until we can't find a title field
-parseTracksFromForm :: Form -> Either Text [TrackInfo]
-parseTracksFromForm form = parseTracksFromIndex 0
+      -- \| Convert empty filename FileData to Nothing
+      fileDataToNothing :: Maybe (FileData Mem) -> Maybe (FileData Mem)
+      fileDataToNothing (Just fileData)
+        | Text.null (fdFileName fileData) = Nothing
+        | otherwise = Just fileData
+      fileDataToNothing Nothing = Nothing
+
+-- | Parse track data from multipart form fields like tracks[0][title], tracks[0][artist], etc.
+-- We try to parse tracks by checking indices starting from 0 until we can't find a title field.
+-- This uses the `inputs` list from MultipartData which only contains text inputs, not file data.
+parseTracksFromMultipart :: Servant.Multipart.MultipartData Mem -> [TrackInfo]
+parseTracksFromMultipart multipartData = parseTracksFromIndex 0
   where
-    parseTracksFromIndex :: Int -> Either Text [TrackInfo]
-    parseTracksFromIndex idx = do
+    -- Get all inputs as a map for efficient lookup
+    -- This is safer than using lookupInput because we can filter and handle errors
+    inputsMap :: [(Text, Text)]
+    inputsMap = [(Servant.Multipart.iName inp, Servant.Multipart.iValue inp) | inp <- Servant.Multipart.inputs multipartData]
+
+    lookupInputSafe :: Text -> Maybe Text
+    lookupInputSafe key = lookup key inputsMap
+
+    parseTracksFromIndex :: Int -> [TrackInfo]
+    parseTracksFromIndex idx =
       let prefix = "tracks[" <> Text.pack (show idx) <> "]"
           titleKey = prefix <> "[title]"
+       in -- Try to parse title for this index - if it doesn't exist, we're done
+          case lookupInputSafe titleKey of
+            Nothing -> [] -- No more tracks
+            Just _ ->
+              -- Parse this track and remaining tracks
+              parseTrack prefix : parseTracksFromIndex (idx + 1)
 
-      -- Try to parse title for this index - if it doesn't exist, we're done
-      case (Form.parseUnique titleKey form :: Either Text Text) of
-        Left _ -> Right [] -- No more tracks
-        Right _ -> do
-          -- Parse this track
-          track <- parseTrack form idx
-          -- Parse remaining tracks
-          remainingTracks <- parseTracksFromIndex (idx + 1)
-          Right (track : remainingTracks)
-
-    parseTrack :: Form -> Int -> Either Text TrackInfo
-    parseTrack f idx = do
-      let prefix = "tracks[" <> Text.pack (show idx) <> "]"
-          getField field = Form.parseUnique (prefix <> "[" <> field <> "]") f
-          getFieldMaybe field = Right $ either (const Nothing) Just (getField field)
-
-      trackId <- getFieldMaybe "id"
-      trackNumber <- getField "track_number"
-      title <- getField "title"
-      artist <- getField "artist"
-      album <- getFieldMaybe "album"
-      year <- getFieldMaybe "year"
-      duration <- getFieldMaybe "duration"
-      label <- getFieldMaybe "label"
-      isPremiere <- Right $ either (const False) (const True) (getField "is_exclusive_premiere")
-
-      pure
-        TrackInfo
-          { tiId = trackId >>= (fmap EpisodeTrack.Id . readMaybe . Text.unpack),
-            tiTrackNumber = fromMaybe 1 (readMaybe $ Text.unpack trackNumber),
-            tiTitle = title,
-            tiArtist = artist,
-            tiAlbum = emptyToNothing album,
-            tiYear = year >>= readMaybe . Text.unpack,
-            tiDuration = emptyToNothing duration,
-            tiLabel = emptyToNothing label,
-            tiIsExclusive = isPremiere
-          }
+    parseTrack :: Text -> TrackInfo
+    parseTrack prefix =
+      let getField field = fromMaybe "" (lookupInputSafe (prefix <> "[" <> field <> "]"))
+          getFieldMaybe field = lookupInputSafe (prefix <> "[" <> field <> "]")
+          trackIdText = getFieldMaybe "id"
+          trackId = trackIdText >>= (fmap EpisodeTrack.Id . readMaybe . Text.unpack)
+          trackNumber = getField "track_number"
+          title = getField "title"
+          artist = getField "artist"
+          album = getFieldMaybe "album"
+          year = getFieldMaybe "year"
+          duration = getFieldMaybe "duration"
+          label = getFieldMaybe "label"
+          isPremiere = case lookupInputSafe (prefix <> "[is_exclusive_premiere]") of
+            Just _ -> True
+            Nothing -> False
+       in TrackInfo
+            { tiId = trackId,
+              tiTrackNumber = fromMaybe 1 (readMaybe $ Text.unpack trackNumber),
+              tiTitle = title,
+              tiArtist = artist,
+              tiAlbum = emptyToNothing album,
+              tiYear = year >>= readMaybe . Text.unpack,
+              tiDuration = emptyToNothing duration,
+              tiLabel = emptyToNothing label,
+              tiIsExclusive = isPremiere
+            }
 
     emptyToNothing :: Maybe Text -> Maybe Text
     emptyToNothing (Just "") = Nothing
@@ -311,6 +336,12 @@ handler _tracer _showId episodeId _urlSlug cookie (foldHxReq -> hxRequest) editF
                           Log.logInfo "User attempted to edit episode they don't own" episode.id
                           renderTemplate hxRequest (Just userMetadata) forbiddenTemplate
 
+-- | Check if the episode's scheduled date is in the future (allowing file uploads)
+isScheduledInFuture :: UTCTime -> Episodes.Model -> Bool
+isScheduledInFuture now episode = case episode.scheduledAt of
+  Nothing -> True -- No scheduled date means it's still editable
+  Just scheduledAt -> scheduledAt > now
+
 updateEpisode ::
   ( MonadDB m,
     Log.MonadLog m,
@@ -318,6 +349,7 @@ updateEpisode ::
     Has Tracer env,
     MonadUnliftIO m,
     MonadCatch m,
+    MonadIO m,
     Has HSQL.Pool.Pool env
   ) =>
   HxRequest ->
@@ -364,15 +396,82 @@ updateEpisode hxRequest _user userMetadata episode showModel editForm = do
               Log.logInfo "Episode update returned Nothing" episode.id
               renderTemplate hxRequest (Just userMetadata) (errorTemplate "Failed to update episode. Please try again.")
             Right (Just _) -> do
-              -- Update tracks
-              updateTracksResult <- updateTracks episode.id (eefTracks editForm)
-              case updateTracksResult of
-                Left trackErr -> do
-                  Log.logInfo "Failed to update tracks" (episode.id, trackErr)
-                  renderTemplate hxRequest (Just userMetadata) (errorTemplate $ "Episode updated but track update failed: " <> trackErr)
-                Right _ -> do
-                  Log.logInfo "Successfully updated episode and tracks" episode.id
-                  renderTemplate hxRequest (Just userMetadata) (successTemplate showModel.id episode.id episode.slug)
+              -- Check if file uploads are allowed (scheduled date is in the future)
+              currentTime <- liftIO getCurrentTime
+              let allowFileUpload = isScheduledInFuture currentTime episode
+
+              -- Process file uploads if allowed and provided
+              fileUpdateResult <-
+                if allowFileUpload && (isJust (eefAudioFile editForm) || isJust (eefArtworkFile editForm))
+                  then processFileUploads showModel episode editForm
+                  else pure $ Right (Nothing, Nothing)
+
+              case fileUpdateResult of
+                Left fileErr -> do
+                  Log.logInfo "Failed to upload files" (episode.id, fileErr)
+                  renderTemplate hxRequest (Just userMetadata) (errorTemplate $ "Episode updated but file upload failed: " <> fileErr)
+                Right (mAudioPath, mArtworkPath) -> do
+                  -- Update file paths in database if any files were uploaded
+                  when (isJust mAudioPath || isJust mArtworkPath) $ do
+                    let fileUpdate =
+                          Episodes.FileUpdate
+                            { efuId = episode.id,
+                              efuAudioFilePath = mAudioPath,
+                              efuArtworkUrl = mArtworkPath
+                            }
+                    execQuerySpan (Episodes.updateEpisodeFiles fileUpdate) >>= \case
+                      Left err -> Log.logInfo "Failed to update file paths" (episode.id, show err)
+                      Right Nothing -> Log.logInfo "File path update returned Nothing" episode.id
+                      Right (Just _) -> Log.logInfo "Successfully updated file paths" episode.id
+
+                  -- Update tracks
+                  updateTracksResult <- updateTracks episode.id (eefTracks editForm)
+                  case updateTracksResult of
+                    Left trackErr -> do
+                      Log.logInfo "Failed to update tracks" (episode.id, trackErr)
+                      renderTemplate hxRequest (Just userMetadata) (errorTemplate $ "Episode updated but track update failed: " <> trackErr)
+                    Right _ -> do
+                      Log.logInfo "Successfully updated episode and tracks" episode.id
+                      renderTemplate hxRequest (Just userMetadata) (successTemplate showModel.id episode.id episode.slug)
+
+-- | Process file uploads for episode editing
+processFileUploads ::
+  ( MonadIO m,
+    Log.MonadLog m
+  ) =>
+  Shows.Model ->
+  Episodes.Model ->
+  EpisodeEditForm ->
+  m (Either Text (Maybe Text, Maybe Text))
+processFileUploads showModel episode editForm = do
+  -- Process audio file if provided
+  audioResult <- case eefAudioFile editForm of
+    Nothing -> pure $ Right Nothing
+    Just audioFile -> do
+      result <- FileUpload.uploadEpisodeAudio showModel.slug episode.slug episode.scheduledAt audioFile
+      case result of
+        Left err -> do
+          Log.logInfo "Failed to upload audio file" (Text.pack $ show err)
+          pure $ Left "Failed to upload audio file"
+        Right uploadResult ->
+          pure $ Right $ Just $ stripStorageRoot $ uploadResultStoragePath uploadResult
+
+  -- Process artwork file if provided
+  artworkResult <- case eefArtworkFile editForm of
+    Nothing -> pure $ Right Nothing
+    Just artworkFile -> do
+      result <- FileUpload.uploadEpisodeArtwork showModel.slug episode.slug episode.scheduledAt artworkFile
+      case result of
+        Left err -> do
+          Log.logInfo "Failed to upload artwork file" (Text.pack $ show err)
+          pure $ Left $ Text.pack $ show err
+        Right uploadResult ->
+          pure $ Right $ Just $ stripStorageRoot $ uploadResultStoragePath uploadResult
+
+  case (audioResult, artworkResult) of
+    (Left audioErr, _) -> pure $ Left audioErr
+    (Right _audioPath, Left artworkErr) -> pure $ Left artworkErr
+    (Right audioPath, Right artworkPath) -> pure $ Right (audioPath, artworkPath)
 
 --------------------------------------------------------------------------------
 -- Track Update Logic
