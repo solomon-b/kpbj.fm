0a5ea0174a0e4a8276dfdda45a083c04903d47d2
solomon <ssbothwell@gmail.com>
2025-12-29 16:48:51 -0800

    fix: Donation Page mobile rendering.

--- src/API/Donate/Get/Templates.hs
+++ src/API/Donate/Get/Templates.hs
@@ -15,6 +15,6 @@ import Lucid qualified
 template :: Lucid.Html ()
 template = do
   pageHeader "SUPPORT KPBJ"
-  Lucid.div_ [Lucid.id_ "paypal-container-YBRDJJA6AGBL6", Lucid.class_ "w-96"] ""
+  Lucid.div_ [Lucid.id_ "paypal-container-YBRDJJA6AGBL6", Lucid.class_ "w-full max-w-96"] ""
   -- Load PayPal SDK if not already loaded
   Lucid.script_ [] ("if (typeof paypal === 'undefined') { const script = document.createElement('script'); script.src = 'https://www.paypal.com/sdk/js?client-id=BAA56A9gcLxsoKcdDF1ipwRFhXfp8nondkp4mIJClY5tiW5pFR9C1kMlHNKYFrRYTeecQ-MzTmxkjAkFFQ&components=hosted-buttons&enable-funding=venmo&currency=USD'; script.onload = function() { paypal.HostedButtons({ hostedButtonId: 'YBRDJJA6AGBL6' }).render('#paypal-container-YBRDJJA6AGBL6'); }; document.head.appendChild(script); } else { paypal.HostedButtons({ hostedButtonId: 'YBRDJJA6AGBL6' }).render('#paypal-container-YBRDJJA6AGBL6'); }" :: Text)
