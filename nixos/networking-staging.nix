138b0f3c41e24bc1a31a7ed619c2122702b17fa5
solomon <ssbothwell@gmail.com>
2026-02-11 18:48:05 -0800

    infra: Rebuilt droplets.

--- nixos/networking-staging.nix
+++ nixos/networking-staging.nix
@@ -2,10 +2,9 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [
-      "8.8.8.8"
-    ];
-    defaultGateway = "164.92.96.1";
+    nameservers = [ "8.8.8.8"
+ ];
+    defaultGateway = "146.190.144.1";
     defaultGateway6 = {
       address = "";
       interface = "eth0";
@@ -15,27 +14,27 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          { address = "164.92.101.240"; prefixLength = 19; }
-          { address = "10.48.0.6"; prefixLength = 16; }
+          { address="146.190.147.93"; prefixLength=20; }
+{ address="10.48.0.6"; prefixLength=16; }
         ];
         ipv6.addresses = [
-          { address = "fe80::5453:71ff:fe84:2895"; prefixLength = 64; }
+          { address="fe80::878:6ff:fe41:2272"; prefixLength=64; }
         ];
-        ipv4.routes = [{ address = "164.92.96.1"; prefixLength = 32; }];
-        ipv6.routes = [{ address = ""; prefixLength = 128; }];
+        ipv4.routes = [ { address = "146.190.144.1"; prefixLength = 32; } ];
+        ipv6.routes = [ { address = ""; prefixLength = 128; } ];
       };
-      eth1 = {
+            eth1 = {
         ipv4.addresses = [
-          { address = "10.124.0.7"; prefixLength = 20; }
+          { address="10.124.0.3"; prefixLength=20; }
         ];
         ipv6.addresses = [
-          { address = "fe80::10d4:28ff:fed1:53b3"; prefixLength = 64; }
+          { address="fe80::d00d:a0ff:fed9:bcbb"; prefixLength=64; }
         ];
-      };
+        };
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="56:53:71:84:28:95", NAME="eth0"
-    ATTR{address}=="12:d4:28:d1:53:b3", NAME="eth1"
+    ATTR{address}=="0a:78:06:41:22:72", NAME="eth0"
+    ATTR{address}=="d2:0d:a0:d9:bc:bb", NAME="eth1"
   '';
 }
