65798ce4b523619bd3edaf535f7c5c38a6c8897f
solomon <ssbothwell@gmail.com>
2026-02-13 16:08:09 -0800

    chore: Apply nix formatting.

--- nixos/networking-prod.nix
+++ nixos/networking-prod.nix
@@ -2,8 +2,9 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "8.8.8.8"
- ];
+    nameservers = [
+      "8.8.8.8"
+    ];
     defaultGateway = "143.110.144.1";
     defaultGateway6 = {
       address = "";
@@ -14,23 +15,23 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          { address="143.110.149.232"; prefixLength=20; }
-{ address="10.48.0.7"; prefixLength=16; }
+          { address = "143.110.149.232"; prefixLength = 20; }
+          { address = "10.48.0.7"; prefixLength = 16; }
         ];
         ipv6.addresses = [
-          { address="fe80::c0af:13ff:fea7:c4df"; prefixLength=64; }
+          { address = "fe80::c0af:13ff:fea7:c4df"; prefixLength = 64; }
         ];
-        ipv4.routes = [ { address = "143.110.144.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = ""; prefixLength = 128; } ];
+        ipv4.routes = [{ address = "143.110.144.1"; prefixLength = 32; }];
+        ipv6.routes = [{ address = ""; prefixLength = 128; }];
       };
-            eth1 = {
+      eth1 = {
         ipv4.addresses = [
-          { address="10.124.0.4"; prefixLength=20; }
+          { address = "10.124.0.4"; prefixLength = 20; }
         ];
         ipv6.addresses = [
-          { address="fe80::40a5:e8ff:fec1:613d"; prefixLength=64; }
+          { address = "fe80::40a5:e8ff:fec1:613d"; prefixLength = 64; }
         ];
-        };
+      };
     };
   };
   services.udev.extraRules = ''
