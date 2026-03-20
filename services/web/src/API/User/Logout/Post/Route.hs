84fa5c02cca1e77a3e511fc4cf0e47e072875a4a
solomon <ssbothwell@gmail.com>
2026-03-20 15:41:54 -0700

    fix: Logout 401 error.

--- services/web/src/API/User/Logout/Post/Route.hs
+++ services/web/src/API/User/Logout/Post/Route.hs
@@ -3,6 +3,7 @@ module API.User.Logout.Post.Route where
 --------------------------------------------------------------------------------
 
 import Data.Text (Text)
+import Domain.Types.Cookie (Cookie)
 import Lucid qualified
 import Servant ((:>))
 import Servant qualified
@@ -12,7 +13,7 @@ import Text.HTML (HTML)
 
 -- | "POST /user/logout"
 type Route =
-  Servant.AuthProtect "cookie-auth"
-    :> "user"
+  "user"
     :> "logout"
+    :> Servant.Header "Cookie" Cookie
     :> Servant.PostAccepted '[HTML] (Servant.Headers '[Servant.Header "Set-Cookie" Text, Servant.Header "HX-Redirect" Text] (Lucid.Html ()))
