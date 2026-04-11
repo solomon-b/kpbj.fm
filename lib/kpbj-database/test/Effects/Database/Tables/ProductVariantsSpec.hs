f5312fd14f10f230a02bf0d39a2df48735a9405f
solomon <ssbothwell@gmail.com>
2026-04-11 09:28:49 -0700

    feat: Inventory decrement/restore queries for Products and ProductVariants.

--- lib/kpbj-database/test/Effects/Database/Tables/ProductVariantsSpec.hs
+++ lib/kpbj-database/test/Effects/Database/Tables/ProductVariantsSpec.hs
@@ -10,11 +10,13 @@ import Hasql.Transaction qualified as TRX
 import Hasql.Transaction.Sessions qualified as TRX
 import Hedgehog (PropertyT, (===))
 import Hedgehog qualified
+import Hedgehog.Gen qualified as Gen
 import Hedgehog.Internal.Property (forAllT)
+import Hedgehog.Range qualified as Range
 import Test.Database.Helpers (insertTestProduct, unwrapInsert)
 import Test.Database.Monad (TestDBConfig, bracketConn, withTestDB)
 import Test.Database.Property (act, arrange, assert, runs)
-import Test.Database.Property.Assert (assertJust, assertRight)
+import Test.Database.Property.Assert (assertJust, assertNothing, assertRight)
 import Test.Gen.Tables.Products (productInsertGen)
 import Test.Gen.Tables.ProductVariants (productVariantInsertGen)
 import Test.Hspec (Spec, describe, it)
@@ -46,6 +48,16 @@ spec =
           hedgehog . prop_softDeleteVariant
         runs 10 . it "softDeleteVariant: getById still returns the variant" $
           hedgehog . prop_softDeleteVariantStillExists
+
+      describe "Inventory" $ do
+        runs 10 . it "decrementInventory succeeds when sufficient stock" $
+          hedgehog . prop_decrementInventorySufficient
+        runs 10 . it "decrementInventory fails when insufficient stock" $
+          hedgehog . prop_decrementInventoryInsufficient
+        runs 10 . it "decrementInventory fails on soft-deleted variant" $
+          hedgehog . prop_decrementInventorySoftDeleted
+        runs 10 . it "restoreInventory increases inventory count" $
+          hedgehog . prop_restoreInventory
 
 --------------------------------------------------------------------------------
 -- Helpers
@@ -205,3 +217,102 @@ prop_softDeleteVariantStillExists cfg = do
         UUT.pvId selected === variantId
         -- deleted_at should be set
         Hedgehog.assert (isJust (UUT.pvDeletedAt selected))
+
+--------------------------------------------------------------------------------
+-- Inventory tests
+
+prop_decrementInventorySufficient :: TestDBConfig -> PropertyT IO ()
+prop_decrementInventorySufficient cfg = do
+  arrange (bracketConn cfg) $ do
+    prodTemplate <- forAllT productInsertGen
+    varTemplate <- forAllT (productVariantInsertGen dummyProductId)
+    stock <- forAllT $ Gen.int64 (Range.linear 2 10000)
+    qty <- forAllT $ Gen.int64 (Range.linear 1 (stock - 1))
+
+    act $ do
+      result <- runDB $ TRX.transaction TRX.ReadCommitted TRX.Write $ do
+        productId <- insertTestProduct prodTemplate
+        let varInsert = varTemplate {UUT.viProductId = productId, UUT.viInventoryCount = stock}
+        variantId <- unwrapInsert (UUT.insertVariant varInsert)
+        decremented <- TRX.statement () (UUT.decrementInventory variantId qty)
+        selected <- TRX.statement () (UUT.getById variantId)
+        TRX.condemn
+        pure (variantId, decremented, selected)
+
+      assert $ do
+        (variantId, mDecremented, mSelected) <- assertRight result
+        decrementedId <- assertJust mDecremented
+        decrementedId === variantId
+        selected <- assertJust mSelected
+        UUT.pvInventoryCount selected === stock - qty
+
+
+prop_decrementInventoryInsufficient :: TestDBConfig -> PropertyT IO ()
+prop_decrementInventoryInsufficient cfg = do
+  arrange (bracketConn cfg) $ do
+    prodTemplate <- forAllT productInsertGen
+    varTemplate <- forAllT (productVariantInsertGen dummyProductId)
+    stock <- forAllT $ Gen.int64 (Range.linear 0 100)
+    extra <- forAllT $ Gen.int64 (Range.linear 1 100)
+
+    act $ do
+      result <- runDB $ TRX.transaction TRX.ReadCommitted TRX.Write $ do
+        productId <- insertTestProduct prodTemplate
+        let varInsert = varTemplate {UUT.viProductId = productId, UUT.viInventoryCount = stock}
+        variantId <- unwrapInsert (UUT.insertVariant varInsert)
+        decremented <- TRX.statement () (UUT.decrementInventory variantId (stock + extra))
+        selected <- TRX.statement () (UUT.getById variantId)
+        TRX.condemn
+        pure (decremented, selected, stock)
+
+      assert $ do
+        (mDecremented, mSelected, originalStock) <- assertRight result
+        assertNothing mDecremented
+        selected <- assertJust mSelected
+        UUT.pvInventoryCount selected === originalStock
+
+
+prop_decrementInventorySoftDeleted :: TestDBConfig -> PropertyT IO ()
+prop_decrementInventorySoftDeleted cfg = do
+  arrange (bracketConn cfg) $ do
+    prodTemplate <- forAllT productInsertGen
+    varTemplate <- forAllT (productVariantInsertGen dummyProductId)
+    stock <- forAllT $ Gen.int64 (Range.linear 1 10000)
+
+    act $ do
+      result <- runDB $ TRX.transaction TRX.ReadCommitted TRX.Write $ do
+        productId <- insertTestProduct prodTemplate
+        let varInsert = varTemplate {UUT.viProductId = productId, UUT.viInventoryCount = stock}
+        variantId <- unwrapInsert (UUT.insertVariant varInsert)
+        _ <- TRX.statement () (UUT.softDeleteVariant variantId)
+        decremented <- TRX.statement () (UUT.decrementInventory variantId 1)
+        TRX.condemn
+        pure decremented
+
+      assert $ do
+        mDecremented <- assertRight result
+        assertNothing mDecremented
+
+
+prop_restoreInventory :: TestDBConfig -> PropertyT IO ()
+prop_restoreInventory cfg = do
+  arrange (bracketConn cfg) $ do
+    prodTemplate <- forAllT productInsertGen
+    varTemplate <- forAllT (productVariantInsertGen dummyProductId)
+    initialStock <- forAllT $ Gen.int64 (Range.linear 0 5000)
+    restoreQty <- forAllT $ Gen.int64 (Range.linear 1 5000)
+
+    act $ do
+      result <- runDB $ TRX.transaction TRX.ReadCommitted TRX.Write $ do
+        productId <- insertTestProduct prodTemplate
+        let varInsert = varTemplate {UUT.viProductId = productId, UUT.viInventoryCount = initialStock}
+        variantId <- unwrapInsert (UUT.insertVariant varInsert)
+        TRX.statement () (UUT.restoreInventory variantId restoreQty)
+        selected <- TRX.statement () (UUT.getById variantId)
+        TRX.condemn
+        pure selected
+
+      assert $ do
+        mSelected <- assertRight result
+        selected <- assertJust mSelected
+        UUT.pvInventoryCount selected === initialStock + restoreQty
